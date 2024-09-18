078b9d19a38b8e321495753c1fee2670b7dbb96f
nfoithong <n.foithong1983@gmail.com>
2024-09-18 15:54:13 -0400

    complete install dependencies frontend

--- frontend/Eagles/src/App.jsx
+++ frontend/Eagles/src/App.jsx
@@ -13,22 +13,11 @@ function App() {
         <a href="#" target="_blank">
           <img src={eagle_logo} className="logo" alt="Eagle logo" />
         </a>
-        {/* <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
+
       </div>
-      <h1>Eagle Chatbot</h1>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div> */}
-      <p className="read-the-docs">
-        BU MET CS Course Building Chatbot (Group 1)
-      </p>
+      <h1>BU Academic Navigator</h1>
+      <h1>It is BUAN chatbot</h1>
+      <p className="read-the-docs">Your own chat buddy</p>
     </>
   )
 }
