5d970949033701ff6f4d59a62c6641bffebf6ea5
Natasya Liew <igotnowifi@igotnowifi-notintelsad.local>
2024-10-12 12:17:47 -0400

    Updated Redux and added Security, after integration test, one more PR for cleaning and annotation -- recommit for linting rules4

--- code/frontend/src/main.jsx
+++ code/frontend/src/main.jsx
@@ -5,7 +5,7 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux'; // Import Provider from react-redux
 import App from './App.jsx';
-import store from '../store/store'; // Import your Redux store
+import store from './store/store'; // Import your Redux store
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
